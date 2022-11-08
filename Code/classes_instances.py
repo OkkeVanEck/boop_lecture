030bfb9497f8183e34af9e37c1809a3aaa8c1205
Okke van Eck <okke.van.eck@gmail.com>
2022-11-08 09:21:05 +0100

    Revised images and slides

--- Code/classes_instances.py
+++ Code/classes_instances.py
@@ -1,23 +1,24 @@
-# Example class instance implementation.
+"""
+Example class instance implementation.
+"""
 
 
 class House:
-	def __init__(self, location, price):
-		self.location = location
-		self.price = price
+    def __init__(self, location, price):
+        self.location = location
+        self.price = price
 
 
 def get_house():
-	location = input("City: ")
-	price = input("Price: ")
-	return House(location, price)
+    location = input("City: ")
+    price = input("Price: ")
+    return House(location, price)
 
 
 def main():
-    house = get_house()  
+    house = get_house()
     print(f"{house.location} for {house.price}")
 
 
 if __name__ == "__main__":
     main()
-
